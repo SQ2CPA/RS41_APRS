bc80e6cc2147c3d0a7e675217a1779a40fc07122
SQ2CPA <sq2cpa@gmail.com>
2025-02-07 18:31:22 +0100

    improvement: HSI 8 MHz for STM32L

--- custom_variants/STM32L051K8U6/generic_clock.c
+++ custom_variants/STM32L051K8U6/generic_clock.c
@@ -34,7 +34,7 @@ WEAK void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_4;
+  RCC_OscInitStruct.PLL.PLLDIV = RCC_PLLDIV_2;
 
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
     Error_Handler();
